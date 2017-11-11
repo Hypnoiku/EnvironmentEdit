736afc3ecc3bfeb03ebbc1bb7e31270fa4946784
Hypnoiku <sonic74523@gmail.com>
2017-11-10 19:16:18 -0600

    Add finding and returning variables in lists. Add private function to set edit bool.

--- EnvironmentEdit/VariableController.cs
+++ EnvironmentEdit/VariableController.cs
@@ -12,7 +12,7 @@ namespace EnvironmentEdit
         {
             List<Variable> variables = new List<Variable>();
 
-            for(int i = 0; i < NameStrings.Length; i++)
+            for (int i = 0; i < NameStrings.Length; i++)
             {
                 String[] vDataStrings = DataStrings[i].Split(';');
                 List<Data> varData = new List<Data>();
@@ -27,6 +27,50 @@ namespace EnvironmentEdit
                 variables.Add(variable);
             }
             return variables;
+        }
+
+        public Variable FindVariableInList(String Name, List<Variable> variables, bool SetEdit)
+        {
+            for (int i = 0; i < variables.Count; i++)
+            {
+                if (Name == variables[i].Name)
+                {
+                    if (SetEdit)
+                    {
+                        variables[i] = SetEditor(variables[i], true);
+                    }
+                    else
+                    {
+                        return variables[i];
+                    }
+
+                }
+            }
+
+            return new Variable("ERR_VAR_NOT_FOUND");
+        }
+
+        public List<Variable> ReturnVariableToList(List<Variable> variables, Variable variable)
+        {
+            for (int i = 0; i < variables.Count; i++)
+            {
+                if (variables[i].Editing = true)
+                {
+                    variable = SetEditor(variable, false);
+                    variables[i] = variable;
+                    return variables;
+                }
+            }
+
+            List<Variable> errNotFound = new List<Variable>();
+            errNotFound.Add(new Variable("ERR_VAR_NOT_FOUND"));
+            return errNotFound;
+        }
+
+        private Variable SetEditor(Variable variable, bool EditorStatus)
+        {
+            variable.Editing = EditorStatus;
+            return variable;
         }
     }
 }
