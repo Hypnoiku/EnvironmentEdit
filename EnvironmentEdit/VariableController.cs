4e00ad3f17ae8ae547474bbbc3e70c58d690b0da
Hypnoiku <sonic74523@gmail.com>
2017-11-22 16:23:51 -0600

    Added functions to modify vars and lists and delete vars.

--- EnvironmentEdit/VariableController.cs
+++ EnvironmentEdit/VariableController.cs
@@ -29,13 +29,13 @@ namespace EnvironmentEdit
             return variables;
         }
 
-        public List<Variable> FindVariableSetEdit(String Name, List<Variable> variables)
+        public List<Variable> SetEdit(String Name, List<Variable> variables, bool EditorStatus)
         {
             for (int i = 0; i < variables.Count; i++)
             {
                 if (Name == variables[i].Name)
                 {
-                    variables[i] = SetEditor(variables[i], true);
+                    variables[i] = SetEditor(variables[i], EditorStatus);
                     return variables;
                 }
             }
@@ -62,7 +62,6 @@ namespace EnvironmentEdit
             {
                 if (variables[i].Editing)
                 {
-                    variable = SetEditor(variable, false);
                     variables[i] = variable;
                     return variables;
                 }
@@ -82,6 +81,71 @@ namespace EnvironmentEdit
             List<Variable> errNotFound = new List<Variable>();
             errNotFound.Add(new Variable("ERR_VAR_NOT_FOUND"));
             return errNotFound;
+        }
+
+        public List<Variable> DeleteVar(List<Variable> variables, Variable variable)
+        {
+            for (int i = 0; i < variables.Count; i++)
+            {
+                if (variable.Name == variables[i].Name)
+                {
+                    variables.RemoveAt(i);
+                }
+            }
+
+            return variables;
+        }
+
+        public int FindPosInList(List<Variable> variables, Variable variable)
+        {
+            for(int i = 0; i < variables.Count; i++)
+            {
+                if (variable.Name == variables[i].Name)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public List<Variable> insertAt(List<Variable> variables, Variable variable, int pos)
+        {
+            variables.Insert(pos, variable);
+            return variables;
+        }
+
+        public Variable SetName(Variable variable, string name)
+        {
+            variable.Name = name;
+            return variable;
+        }
+
+        public Variable SetData(Variable variable, string RawData)
+        {
+            variable.Data.Clear();
+            string data = null;
+            for (int i = 0; i < RawData.Length; i++)
+            {
+                if (RawData[i] == ';')
+                {
+                    Data varData = new Data(data);
+                    variable.Data.Add(varData);
+                    data = null;
+                }
+                else
+                {
+                    data += RawData[i];
+
+                    if (i == RawData.Length - 1)
+                    {
+                        Data varData = new Data(data);
+                        variable.Data.Add(varData);
+                    }
+                }
+            }
+
+            return variable;
         }
     }
 }
